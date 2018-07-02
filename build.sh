a2704aec98a2d986f73278295831780a6b258664
Mike Nye <mike.nye@xpanse.com.au>
2018-07-02 21:37:23 +0800

    updates

--- build.sh
+++ build.sh
@@ -3,18 +3,23 @@
 IMAGENAME="livehouseautomation/veraflux-grafana"
 TAG="development-v5.1.x"
 
+echo "GENERATING DOCKERFILES FROM Dockerfile.in"
 python ./helper_scripts/generate_dockerfiles.py
 
 # set up multiarch build env
+echo "SETTING UP MULTI ARCH BUILD ENVIRONMENT"
 sudo docker run --rm --privileged multiarch/qemu-user-static:register --reset
 
 # build arm32v7
+echo "BUILDING arm32v7"
 docker build -t $IMAGENAME:$TAG-arm32v7 ./arm32v7/
 
 # build amd64
+echo "BUILDING amd64"
 docker build -t $IMAGENAME:$TAG-amd64 ./amd64/
 
 # Create multiarch manifest
+echo "CREATE MULTI ARCH MANIFEST"
 # Docker architectures
 # https://github.com/docker-library/official-images/blob/a7ad3081aa5f51584653073424217e461b72670a/bashbrew/go/vendor/src/github.com/docker-library/go-dockerlibrary/architecture/oci-platform.go#L14-L25
 #
@@ -23,6 +28,7 @@ docker manifest annotate $IMAGENAME:$TAG $IMAGENAME:$TAG-amd64 --os linux --arch
 docker manifest annotate $IMAGENAME:$TAG $IMAGENAME:$TAG-arm32v7 --os linux --arch arm --variant v7
 
 # push everything
+echo "PUSHING IMAGES & MANIFEST TO DOCKER HUB"
 docker push $IMAGENAME:$TAG-arm32v7
 docker push $IMAGENAME:$TAG-amd64
 docker manifest push $IMAGENAME:$TAG
