f89e3f826347e98d1a24b1f38f471f69aa7cbb46
Mike Nye <mike.nye@xpanse.com.au>
2018-07-02 22:43:00 +0800

    updates
    install go directly

--- helper_scripts/generate_dockerfiles.py
+++ helper_scripts/generate_dockerfiles.py
@@ -2,14 +2,21 @@
 
 import os
 
-arch_images = {'amd64':   {'image': 'amd64/ubuntu:bionic',   'target': 'amd64'},
-               'arm32v7': {'image': 'arm32v7/ubuntu:bionic', 'target': 'armhf'},
+arch_images = {'amd64':   {'image':  'amd64/ubuntu:bionic',   
+                           'target': 'amd64', 
+                           'goarch': 'amd64',
+                           'gourl':  'https://dl.google.com/go/go1.10.3.linux-amd64.tar.gz',},
+               'arm32v7': {'image':  'arm32v7/ubuntu:bionic', 
+                           'target': 'armhf', 
+                           'goarch': 'arm'
+                           'gourl':  'https://dl.google.com/go/go1.10.3.linux-armv6l.tar.gz'},
               }
 
 for arch in arch_images.keys():
 	baseimage = arch_images[arch]['image']
 	target = arch_images[arch]['target']
 	outputfile = os.path.join(os.getcwd(),arch,"Dockerfile")
+	gourl = arch_images[arch]['gourl']
 
 	print("Making Dockerfile '%s' for %s using base image of %s for a target of %s" % 
 		(outputfile, arch, baseimage, target))
@@ -18,6 +25,7 @@ for arch in arch_images.keys():
 		read_data = dockerfile_in.read()
 		read_data = read_data.replace("%BASEIMAGE%", baseimage)
 		read_data = read_data.replace("%TARGET%", target)
+		read_data = read_data.replace("%GOURL%", gourl)
 		with open(outputfile, 'w') as dockerfile_out:
 			dockerfile_out.write(read_data)
 
