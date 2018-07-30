55aeb9a73a15db984e4fa3437a4050758e54cf17
Mike Nye <mike.nye@xpanse.com.au>
2018-07-30 18:43:14 +0800

    changes

--- arm32v7/build.sh
+++ arm32v7/build.sh
@@ -1,11 +1,8 @@
-
 #!/bin/bash
 
 ARCH="arm32v7"
 IMAGENAME="livehouseautomation/veraflux-grafana"
-TAG="dev-5.1.x"
+TAG="l"
 
 echo "BUILDING $ARCH"
 docker build -t $IMAGENAME:$TAG-$ARCH .
-docker push $IMAGENAME:$TAG-$ARCH
-		
